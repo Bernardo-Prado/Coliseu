44a36f4b11878a233045d7ee33f0310889b29d1b
Bernardo-Prado <bernardoprado22@yahoo.com.br>
2022-05-04 12:19:55 -0300

    Add enemy and player

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position) {
+        this.position = position
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+const enemy = new Sprite({
+    x: 400,
+    y: 100
+})
+
+player.draw()
+enemy.draw()
