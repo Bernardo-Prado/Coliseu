402fe430b6530ac762fa2c1e2db0ca0918cb224e
Bernardo-Prado <bernardoprado22@yahoo.com.br>
2022-05-10 16:16:34 -0300

    add background

--- js/classes.js
+++ js/classes.js
@@ -1,14 +1,23 @@
 class Sprite {
     // parametros que estão sendo passados no constructor são dinâmicos
     constructor({
-        position
+        position,
+        imageSrc
     }) {
         this.position = position;
         this.width = 50;
         this.height = 150;
+        this.image = new Image()
+        this.image.src = imageSrc
     }
 
-    draw() {}
+    draw() {
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y
+        )
+    }
 
     update() {
         this.draw();
